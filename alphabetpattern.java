b15160bf3b1ea43554c403ad61dc610d537a71ed
Divya-1810 <146337756+Divya-1810@users.noreply.github.com>
2025-01-21 12:55:44 +0530

    Add files via upload

---
+++ alphabetpattern.java
@@ -0,0 +1,13 @@
+public class alphabetpattern{
+public static void main(String[ ] args){
+int row=4;
+int col=4;
+char ch='a';
+for(int i=1;i<=row;i++){
+for(int j=1;j<=col;j++){
+System.out.print(ch++ +" ");
+}
+System.out.println( );
+}
+}
+}
